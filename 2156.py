765c7901fa60e06010dd0b575c887f212c55f89d
Park jiwon <pannchat@likelion.org>
2021-03-12 17:04:39 +0900

    algor

--- 2156.py
+++ 2156.py
@@ -8,7 +8,7 @@ if n > 1 :
     dp.append(wine[1]+wine[2])
 
 for i in range(3,n+1):
-    case1 = wine[i] + dp[i-2]
+    case1 = wine[i] + dp[i-2] 
     case2 = wine[i] + wine[i-1] + dp[i-3]
     case3 = dp[i-1]
     dp.append(max(case1,case2,case3))
