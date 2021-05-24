fc7dd921d445f0be527d07812c9c98098559fe38
Park jiwon <pannchat@likelion.org>
2021-05-24 14:04:43 +0900

    인접행렬

---
+++ python_algorithm/section6/12.py
@@ -0,0 +1,13 @@
+
+
+if __name__ == "__main__":
+    n,m = map(int,input().split())
+    arr = [[0] * n for i in range(n)]
+
+    for _ in range(m):
+        a,b,c = map(int,input().split())
+        arr[a-1][b-1] = c
+    for i in range(n):
+        for j in range(n):
+            print(arr[i][j], end=' ')
+        print()
