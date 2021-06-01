950872bc54eba94971f61ae243ffa808efa42cee
Park jiwon <pannchat@likelion.org>
2021-06-01 18:13:54 +0900

    BFS

---
+++ python_algorithm/section7/6.py
@@ -0,0 +1,24 @@
+def DFS(L,p):
+    global cnt
+    if L == n:
+        cnt += 1
+        for j in range(p):
+            print(chr(res[j] + 64), end ='')
+        print()
+    else:
+        for i in range(1,27):
+            if code[L] == i:
+                res[p]=i
+                DFS(L+1, p + 1)
+            elif i >= 10 and code[L] == i//10 and code[L+1] == i % 10:
+                res[p] = i
+                DFS(L+2, p + 1)
+                
+if __name__ == "__main__":
+    code = list(map(int,input()))
+    n = len(code)
+    code.insert(n, -1)
+    cnt = 0
+    res= [0] * (n+3)
+    DFS(0,0)
+    print(cnt)
