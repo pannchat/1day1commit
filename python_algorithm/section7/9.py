ee8a83c2ee4a125d83ec1d30a29f79e2c83b013f
Park jiwon <pannchat@likelion.org>
2021-06-01 21:09:25 +0900

    DFS

---
+++ python_algorithm/section7/9.py
@@ -0,0 +1,24 @@
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+
+def DFS(x,y):
+    global cnt
+    if x == 6 and y == 6:
+        cnt+=1
+    for i in range(4):
+        xx = x+dx[i]
+        yy = y+dy[i]
+        if 0<= xx <= 6 and 0<= yy <= 6 and arr[xx][yy] == 0:
+            arr[xx][yy] = 1
+            DFS(xx,yy)
+            arr[xx][yy] = 0
+if __name__ == "__main__":
+    arr =[]
+
+    for _ in range(7):
+        arr.append(list(map(int,input().split())))
+    cnt = 0
+    arr[0][0] = 1
+
+    DFS(0,0)
+    print(cnt)
