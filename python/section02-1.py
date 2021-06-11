b206fcc8d6422e5b8c55908ecfe6d841d4da6985
pannchat <pannchat@likelion.org>
2021-06-12 00:03:59 +0900

    python

---
+++ python/section02-1.py
@@ -0,0 +1,18 @@
+print('Hello world!')
+print("Hello world!")
+print("""Hello world!""")
+print('''Hello world!''')
+
+print()
+
+print('p','y','t','h','o','n', sep='!')
+
+print("Hi", end=' ')
+print("jiwon", end=' ')
+print()
+
+print('{}@{}'.format('admin','naver.com'))
+print('{0} and {1} and {0}'.format('one','two','three'))
+print("{a} and {b}".format(a='one', b='two'))
+
+print("%s이 가장 좋아하는 숫자는 %d이다" %('jiwon',21))
