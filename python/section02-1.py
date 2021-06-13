0300eab47dc93d8cff154e21c332fe2ffa2d5a96
pannchat <pannchat@likelion.org>
2021-06-13 19:08:51 +0900

    python

--- python/section02-1.py
+++ python/section02-1.py
@@ -16,3 +16,13 @@ print('{0} and {1} and {0}'.format('one','two','three'))
 print("{a} and {b}".format(a='one', b='two'))
 
 print("%s이 가장 좋아하는 숫자는 %d이다" %('jiwon',21))
+print("Test1 : %5d, Price: %4.2f" %(512, 3141.592))
+
+print("Test1 : {0:5d}, Price:{1: 4.2f}".format(512, 3141.592))
+print("Test1 : {a:5d}, Price:{b: 4.2f}".format(a=512, b=3141.592))
+
+
+print("\' 작은 따옴표 사용하기 \'")
+print("\" 큰 따옴표 사용하기 \"")
+print("줄바꿈 \n")
+print("\t 탭")
