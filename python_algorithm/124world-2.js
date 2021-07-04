1b01ea945a481577c139d009567550d8b9da2b03
pannchat <pannchat@likelion.org>
2021-07-04 23:42:35 +0900

    js

--- python_algorithm/124world-2.js
+++ python_algorithm/124world-2.js
@@ -3,7 +3,7 @@ function solution(n) {
     var answer = ''
     var arr=[1,2,4]
     if (n===0){
-        return ''
+        ret urn ''
     }else{
         return solution(parseInt( ( n-1 ) / 3)) + arr[( n - 1 ) % 3] 
     }
