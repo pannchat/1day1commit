da10bf8912b9e4fa4b288f9e6b2ca618f5cc6382
pannchat <pannchat@likelion.org>
2021-08-11 13:09:37 +0900

    algorithm

--- js/1181.js
+++ js/1181.js
@@ -1,6 +1,13 @@
 let fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const N = input.shift();
-const sorted = input.sort((a, b) => a.length - b.length || a.localeCompare(b));
-const uniqueValues = new Set(sorted);
-console.log(Array.from(uniqueValues).join('\n'));
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = parseInt(input.shift());
+input.sort();
+input.sort((x,y)=>{
+    return x.length-y.length
+})
+
+
+let res = [...new Set(input)];
+res.map((el) => {
+    console.log(el)
+})
