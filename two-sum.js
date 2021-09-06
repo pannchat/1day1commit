d6f53e2d67a7cf82e5f35d9f6d49060be2466738
pannchat <pannchat@likelion.org>
2021-09-06 20:31:47 +0900

    two sum

---
+++ two-sum.js
@@ -0,0 +1,10 @@
+var twoSum = function(nums, target) {
+    const map={};
+   for(let i = 0; i<nums.length; i++){
+       const value = target - nums[i];
+       if(nums[i] in map)
+           return [map[nums[i]],i];
+        map[value] = i
+   }
+
+};
