6a960a8de8b1a3d002a256580f50177eedaa1dd1
pannchat <pannchat@likelion.org>
2021-09-21 22:25:47 +0900

    2108

--- js/2108.js
+++ js/2108.js
@@ -1,18 +1,20 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let arr = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const n = parseInt(input.shift());
-const arr = []
-for(let i = 0; i < n; i++){
-    arr.push(parseInt(input.shift()));
-}
+const n = parseInt(arr.shift());
+
+// const arr = []
+// for(let i = 0; i < n; i++){
+//     arr.push(parseInt(input.shift()));
+// }
+
 
 // 산술평균
-console.log(Math.round(arr.reduce( (a,b) =>a+b,0)/n));
+console.log(Math.round(arr.reduce( (a,b) =>parseInt(a)+parseInt(b),0)/n));
 arr.sort((a,b) => a-b);
 // 중앙값
 console.log(arr[parseInt(n/2)])
-//최빈값
+// 최빈값
 const dict = new Map();
 arr.forEach(e=>{
     if(dict.has(e)) dict.set(e,dict.get(e)+1)
